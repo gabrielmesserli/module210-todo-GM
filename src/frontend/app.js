1cbeb092c939495a2672402950605bdae76ba795
Gabriel <gabriel.messerli@studentfr.ch>
2026-01-27 10:44:02 +0100

    modification apiendpoint

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "http://localhost:8080/api/tasks";
+const apiEndpoint = "http://13.74.244.169:8080/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
