c7b2570af6a36ddbe66b731fd2c3f7a3b84589ff
Gabriel <gabriel.messerli@studentfr.ch>
2026-02-02 16:16:16 +0100

    frontend

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "http://20.82.190.124:8080/api/tasks";
+const apiEndpoint = "https://backend-210.niceground-ac2a196a.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
